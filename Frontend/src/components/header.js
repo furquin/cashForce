1eb4226b0599a40d5c54940fc471aaf825004b5f
furquin <laert.ff@gmail.com>
2022-05-09 19:57:03 -0300

    style: modica css site bar e cria componenteheade

---
+++ Frontend/src/components/header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Header () {
+
+  return (
+    <div className='bg-greenCash w-1272 h-64 ml-288 mt-0 absolute' />
+  );
+}
--- Frontend/src/components/sideBar.js
+++ Frontend/src/components/sideBar.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from '../images/logo.png'
-import maos from '../images/maos.png'
+import maosGreen from '../images/maosGreen.png'
 
 
 export default function DataNf () {
 
   return (
-    <div className='absolute w-288 h-900 ml-0 mt-0 '>
+    <div className='bg-whiteCash ml-0 absolute w-288 h-900 ml-0 mt-0 '>
       <div className='flex flex-col items-center p-0 absolute w-288 h-144'>
         <div className='flex flex-col p-0 static w-288 h-80 ml-0 mt-0 grow-0'>
           <div className="static w-288 h-80 ml-0 mt-0 grow-0">
@@ -14,14 +14,14 @@ export default function DataNf () {
           </div>
         </div>
         <div className="flex flex-col items-start p-0 static w-288 h-64 ml-0 mt-80 grow-0">
-          <div className='flex items-center p-0 static w-149 h-64 ml-0 mt-0 grow-0'>
+          <div className='flex items-center  justify-items-start p-0 static w-149 h-64 ml-0 mt-0 grow-0'>
             <div className='flex items-center px-2.5 static w-4 h-84 mt-n10 grow-0'>
-              <div className='bg-greenCash absolute w-4 h-64 ml-0 mt-10 grow-0' />
+              <div className=' bg-greenCash absolute w-4 h-64 ml-0 mt-10 grow-0' />
             </div>
             <div className='flex items-center p-0 static w-124 h-44 ml-25 mt-10 grow-0'>
               <div className='flex items-center px-2.5 w-24 h-44 ml-0 mt-0 grow-0'>
                 <div className='w-24 h-24 static ml-0 mr-0 mt-p22 mb-p22 grow-0'>
-                  <img className=" absolute w-24 h-14" src={maos} />
+                  <img className=" absolute w-24 h-14" src={maosGreen} alt='mãos verdes' />
                 </div>
               </div>
               <div className='flex content-center mt-n10 px-2.5 static w-90 h-44 ml-39 mt-0 grow' >
